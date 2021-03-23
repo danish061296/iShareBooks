ba777ad7040a2e1f654ddd37056035745087274f
aryannayazmin <60450993+aryannayazmin@users.noreply.github.com>
2021-03-23 10:53:15 -0700

    Registration front end

--- application/vertical-prototype/frontend/src/pages/Registration.js
+++ application/vertical-prototype/frontend/src/pages/Registration.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Register from '../components/Registration';
 
 const Registration = () => {
   return (
     <div>
       <Navigation />
+      <Register />
     </div>
   );
 };
