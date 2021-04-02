3bb98684f2da41b6ea836adc4e17a04dfcfbc9eb
Mark-Jovero <54325578+Mark-Jovero@users.noreply.github.com>
2021-04-01 19:06:11 -0700

    Update dataBase.js

--- application/vertical-prototype/backend/dataBase.js
+++ application/vertical-prototype/backend/dataBase.js
@@ -1,22 +1,12 @@
 const mysql = require('mysql');
 const dotenv= require("dotenv");
 dotenv.config({path: "./.env"});
-var db= mysql.createConnection({
+var db= mysql.createPool({
   host     : process.env.HOST,
   user     : 'root',
   password : process.env.PASSWORD,
   database: process.env.DATABASE,
   multipleStatements:true
-});
-
-
-db.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
- 
-  console.log('connected as id ' + db.threadId);
 });
 
 module.exports=db;
