f0e366db688575a437143e7615dc8ae8d750a96a
Mark Jovero <mjovero@mail.sfsu.edu>
2021-04-14 13:43:30 -0700

    Added a trending query -- to be used for landing page

--- application/vertical-prototype/backend/routes/books.js
+++ application/vertical-prototype/backend/routes/books.js
@@ -9,15 +9,61 @@ router.post('/search', (req, res) => {
   const { searchField } = req.body; // searchType can be: 'any', 'department', 'title', 'author'. Prof wants a pulldown menu with 3 categ for search.
 
   let query;
+	let trending_limit_entries = 100;	
+	
+	/* insert a book into trending table
+			args: book_id (int) the book id of a book
+	*/
+	function trending_insert(book_id) {
+		// limit entries in the trending table
+		query = "SELECT * FROM trending";
+		db.query(query, (err, result) => {
+		
+			if (err)
+				res.status(500).send('error getting the count data from trending', err);
+				
+			else {
+				// has enough space to insert for trending
+				if (result.length < trending_limit_entries) {
+					query = "INSERT INTO trending (book_id) VALUES (" + book_id +")";
+					db.query(query, (err2, result2) => {
+						if (err2)
+							res.status(500).send('error inserting the data into trending', err);
+					});
+				}
+				
+				// too much data, remove the first entry
+				else {
+					console.log("RESULT " + result.length);
+					query = "SELECT MIN(trending_id) as len FROM trending";
+					db.query(query, (err2, result2) => {
+						if (err2)
+							res.status(500).send('error inserting the data into trending', err);
+						else {
+								var min = result2[0].len;
+								// removal of min
+								query = "DELETE FROM trending WHERE trending_id=" + min;
+								db.query(query, (err3, result3) => {
+									if (err3)
+										res.status(500).send('error removing the data from trending', err);
+									else {
+										trending_insert(book_id);
+									}
+									
+								});
+							}
+					});
+				}
+			}
+		
+		});
+	}
+	
+	
 
   function suggestions() {
-    query = 'SELECT * FROM Book ORDER BY title ASC LIMIT 8';
+    query = 'SELECT * FROM paidbooks ORDER BY title ASC LIMIT 8';
     db.query(query, (err, results) => {
-      // let suggest = JSON.stringify(results);
-      //console.log(JSON.parse(suggest))
-      // console.log('Printig Results: ' + suggest)
-
-      // let obj = JSON.parse(suggest);
 
       results.forEach(function (book, index) {
         if (book.image) {
@@ -38,7 +84,7 @@ router.post('/search', (req, res) => {
   //searchField = "Co";
   if (searchType == 'any')
     query =
-      'SELECT * FROM Book WHERE title LIKE ' +
+      'SELECT * FROM paidbooks WHERE title LIKE ' +
       db.escape('%' + searchField + '%') +
       ' OR author LIKE ' +
       db.escape('%' + searchField + '%') +
@@ -48,7 +94,7 @@ router.post('/search', (req, res) => {
     suggestions();
     //  query = "SELECT * FROM BOOK ORDER BY title ASC LIMIT 8";
   } else if (searchType != 'any') {
-    query = 'SELECT * FROM BOOK ORDER BY title ASC LIMIT 8';
+    query = 'SELECT * FROM paidbooks ORDER BY title ASC LIMIT 8';
   }
 
   db.query(query, (err, results) => {
@@ -62,9 +108,7 @@ router.post('/search', (req, res) => {
         }
       });
 
-      console.log(results.length);
       if (results.length > 0) {
-        console.log(results);
         res.send(results);
       } else {
         suggestions();
