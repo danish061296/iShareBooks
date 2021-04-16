bb251a71b81e7394059892d870acf04398f37b3b
Mark Jovero <mjovero@mail.sfsu.edu>
2021-04-15 17:42:38 -0700

    Added post book functionalty

--- application/vertical-prototype/backend/routes/post.js
+++ application/vertical-prototype/backend/routes/post.js
@@ -3,59 +3,29 @@ const db = require("../dataBase.js");
 const router = express.Router();
 // const auth = require("../middleware/auth");
 
-router.post("/posts", (req, res) => {
-  console.log("Dsad");
-  const { description, price, condition } = JSON.parse(req.body.data);
-  if (!req.files) {
-    return res.status(500).send("no file was uplaoded");
+router.post("/post", (req, res) => {
+  const {bookType, bookTitle, bookAuthor, bookCondition, bookImage} = req.body;
+  var query;
+
+  if (bookType == "free" || bookType == "trade")
+    query = "INSERT INTO freebooks (title, author, condition, image) VALUES \
+    ("+db.escape(bookTitle)+", "+db.escape(bookAuthor)+", "+db.escape(bookCondition)+", "+db.escape(bookImage)+")";
+
+  else {
+    const cost = req.body;
+    query = "INSERT INTO freebooks (title, author, condition, image, cost) VALUES \
+    ("+db.escape(bookTitle)+", "+db.escape(bookAuthor)+", "+db.escape(bookCondition)+", "+db.escape(bookImage)+", "+db.escape(cost)+")";
   }
 
-  let imageFile = req.files;
-  let image = imageFile.file;
-  console.log(image);
+  db.query(query, (err, result) => {
+    if (err)
+      throw err;
+    else
+      console.log("Successfully added a book into database.");
+  });
 
-  imageFile.file.mv("../public", function (err) {
-    if (err) {
-      console.log("err");
-      console.log(err);
-      return res.status(500).send(err);
-    }
-    const data = [image, description, price];
-    let inserSql = `INSERT INTO posts (image,description,price,condition) VALUES (?)`;
-    db.query(inserSql, [data], (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send(err);
-      } else {
-        console.log(results);
-      }
-    });
-  });
-});
-router.get("/post/:id", (req, res) => {
-  console.log("dsads");
-  const id = req.params.id;
-  console.log(req.params);
-  let inserSql = `SELECT * FROM posts where id=${id}`;
-  db.query(inserSql, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("error getting the data");
-    } else {
-      res.send(results);
-    }
-  });
 });
 
-router.get("/posts", (req, res) => {
-  let inserSql = `SELECT * FROM posts`;
-  db.query(inserSql, (err, results) => {
-    if (err) {
-      res.status(500).send("error getting the data", err);
-    } else {
-      res.send(results);
-    }
-  });
-});
+
 
 module.exports = router;
