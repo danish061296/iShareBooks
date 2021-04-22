e1f0c905dca921f714dd16bdf4b385dca363434b
Zaid Alkhatib <Zaid.alkhatib97@gmail.com>
2021-04-21 23:28:00 -0700

    trending books

--- application/vertical-prototype/frontend/src/components/Trending.js
+++ application/vertical-prototype/frontend/src/components/Trending.js
@@ -1,36 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Trending.css';
 import Carousel from 'react-elastic-carousel';
 import Card from './Card';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 import defaultImage from './book1.jpg';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-
+import axios from 'axios';
 const Trending = () => {
-  React.useEffect(() => {
-    Aos.init({ duration: 1600 });
+  const [trendingBooks, setTrendingBooks] = useState([]);
+  React.useEffect(async () => {
+    Aos.init({duration: 1600});
+    const res = await axios.get('http://localhost:3001/search', {
+      searchField: '',
+      searchType: 'any',
+    });
+    setTrendingBooks(res.data);
   }, []);
 
   const searchField = useSelector((state) => state.userReducer.searchField);
   const posts = useSelector((state) => state.userReducer.posts);
   const randomMsg = useSelector((state) => state.userReducer.randomMsg);
 
-  console.log(randomMsg);
-
+  console.log(trendingBooks);
   const breakPoints = [
-    { width: 500, itemsTo0how: 1 },
-    { width: 768, itemsToShow: 2 },
-    { width: 1200, itemsToShow: 2 },
-    { width: 1500, itemsToShow: 2 },
+    {width: 500, itemsTo0how: 1},
+    {width: 768, itemsToShow: 2},
+    {width: 1200, itemsToShow: 2},
+    {width: 1500, itemsToShow: 2},
   ];
 
   return (
-    <div className="trending__container" data-aos="fade-right" id="trending">
+    <div className='trending__container' data-aos='fade-right' id='trending'>
       {randomMsg && posts.length > 0 && (
-        <div className="trending__container">
-          <div className="trending__title">
-            <h3 className="trending"> {randomMsg}</h3>
+        <div className='trending__container'>
+          <div className='trending__title'>
+            <h3 className='trending'> {randomMsg}</h3>
           </div>
 
           <Carousel breakPoints={breakPoints}>
@@ -48,9 +53,9 @@ const Trending = () => {
         </div>
       )}
       {!randomMsg && posts.length > 0 && (
-        <div className="trending__container">
-          <div className="trending__title">
-            <h3 className="trending"> Showing Results for {searchField}</h3>
+        <div className='trending__container'>
+          <div className='trending__title'>
+            <h3 className='trending'> Showing Results for {searchField}</h3>
           </div>
 
           <Carousel breakPoints={breakPoints}>
@@ -69,30 +74,22 @@ const Trending = () => {
       )}
 
       {posts.length === 0 && (
-        <div className="trending__container">
-          <div className="trending__title">
-            <h3 className="trending">Trending Books will appear here!</h3>
+        <div className='trending__container'>
+          <div className='trending__title'>
+            <h3 className='trending'>Trending Books!</h3>
           </div>
-          <div className="default__image">
-            <img
-              style={{ width: 300, height: 350 }}
-              className="default__img"
-              src={defaultImage}
-              alt="default_image"
-            />
-            <img
-              style={{ width: 300, height: 350 }}
-              src={defaultImage}
-              className="default__img"
-              alt="default image"
-            />
-            <img
-              style={{ width: 300, height: 350 }}
-              src={defaultImage}
-              className="default__img"
-              alt="default image"
-            />
-          </div>
+          <Carousel breakPoints={breakPoints}>
+            {trendingBooks.map((post, index) => {
+              return (
+                <Card
+                  key={index}
+                  number={index}
+                  image={post.image}
+                  defaultImage={defaultImage}
+                />
+              );
+            })}
+          </Carousel>
         </div>
       )}
     </div>
