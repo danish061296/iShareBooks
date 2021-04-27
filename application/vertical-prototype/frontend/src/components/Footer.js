6058c5f0dfe5d6cc3b67bfbc060325137d5527f9
pramodkhatri10 <pramodkhatri4495@gmail.com>
2021-04-27 00:08:33 -0700

    social icon resposive

--- application/vertical-prototype/frontend/src/components/Footer.js
+++ application/vertical-prototype/frontend/src/components/Footer.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import { animateScroll as scroll } from 'react-scroll';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { animateScroll as scroll } from "react-scroll";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import {
   TiSocialTwitter,
   TiSocialLinkedin,
   TiSocialFacebook,
-} from 'react-icons/ti';
-import './Footer.css';
+} from "react-icons/ti";
+import "./Footer.css";
 
 const Footer = () => {
   return (
@@ -17,7 +18,7 @@ const Footer = () => {
         className="footer"
         color=""
         dark
-        style={{ height: '300px', background: '#18504c' }}
+        style={{ height: "300px", background: "#18504c" }}
       >
         <div className="Fcontent">
           <div className="fcontent">
@@ -30,11 +31,15 @@ const Footer = () => {
             <section className="center">
               Social
               <section>
-                <TiSocialTwitter circle="true" />
-
-                <TiSocialLinkedin />
-
-                <TiSocialFacebook />
+                <a href="https://www.facebook.com/"> 
+                  <FaFacebook />
+                </a>
+                <a href="https://www.instagram.com/">
+                  <FaInstagram />
+                </a>
+                <a href="https://www.linkedin.com/signup/cold-join?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Ffeed%2F&trk=login_reg_redirect">
+                  <FaLinkedin />
+                </a>
               </section>
             </section>
           </div>
