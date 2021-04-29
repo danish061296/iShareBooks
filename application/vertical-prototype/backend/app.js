c454058dc6b1f684400d7fa61ae8e6fe23487b99
Mark Jovero <mjovero@mail.sfsu.edu>
2021-04-29 14:49:47 -0700

    Added server ip to app.js

--- application/vertical-prototype/backend/app.js
+++ application/vertical-prototype/backend/app.js
@@ -7,14 +7,14 @@ const fileUpload = require('express-fileupload');
 var cors = require('cors');
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://35.215.84.127:3000'],
     methods: ['GET', 'POST'],
     credentials: true,
   })
 );
 app.use(fileUpload());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header("Access-Control-Allow-Origin", "http://35.215.84.127:3000/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
