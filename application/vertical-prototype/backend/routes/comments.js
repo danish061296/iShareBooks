4353e3a0759d386e012cab6aa045d6ebc60af9f0
Danish Siddiqui <dsiddiqui@mail.sfsu.edu>
2021-05-11 03:43:30 -0700

    Comment box works for all loggedin users in viewbooks

--- application/vertical-prototype/backend/routes/comments.js
+++ application/vertical-prototype/backend/routes/comments.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const db = require("../dataBase.js");
+const express = require('express');
+const db = require('../dataBase.js');
 const router = express.Router();
 
-router.post("/comments", (req, res) => {
+router.post('/comments', (req, res) => {
   console.log(req.body);
   const { postID, comment, id } = req.body;
   const data = [postID, comment, id];
-  let inserSql = `INSERT INTO comments (postID, comment, id) VALUES (?)`;
+  let inserSql = `INSERT INTO comments (postID, comment, user_id) VALUES (?)`;
   db.query(inserSql, [data], (err, results) => {
     if (err) {
       return res.status(500).send(err);
     } else {
       console.log(results);
+      res.send({ comment: true, results: results });
     }
   });
 });
 
-router.get("/comments/:postID", (req, res) => {
+router.get('/comments/:postID', (req, res) => {
+  console.log(req.params.postID);
   const postID = req.params.postID;
-  let inserSql = `SELECT comments.*, users.name FROM comments JOIN users ON id = user_id where postID=(?) ORDER BY post_time DESC`;
+  let inserSql = `SELECT comments.*, users.name FROM comments JOIN users ON id = user_id where postID=(?) ORDER BY post_time`;
   db.query(inserSql, [postID], (err, results) => {
     if (err) {
       console.log(err);
-      res.status(500).send("error getting the data", err);
+      res.status(500).send('error getting the data', err);
     } else {
       res.send(results);
     }
--- application/vertical-prototype/frontend/src/pages/ViewBook.js
+++ application/vertical-prototype/frontend/src/pages/ViewBook.js
@@ -17,15 +17,20 @@ import {
   setSellerEmail,
   setSellerId,
 } from '../redux/actions/userActions';
+import axios from 'axios';
+import SendIcon from '@material-ui/icons/Send';
 
 const ViewBook = () => {
   // get array object from redux store
   const viewBooks = useSelector((state) => state.userReducer.viewBooks);
   const loggedIn = useSelector((state) => state.userReducer.isLoggedIn);
+  const userid = useSelector((state) => state.userReducer.userid);
 
   const [comment, setComment] = React.useState('');
   const [comments, setComments] = React.useState([]);
-  const [commentDivStyle, setCommentDivStyle] = React.useState({display: '',});
+  const [commentDivStyle, setCommentDivStyle] = React.useState({ display: '' });
+  // const [postID, setPostID] = React.useState(0);
+
   // to dispatch value to redux store
   const dispatch = useDispatch();
 
@@ -46,9 +51,57 @@ const ViewBook = () => {
 
   const handleComment = () => {
     console.log(comment);
-    setComments([...comments, comment]);
+
+    console.log(viewBooks[viewBooks.length - 1].id);
+    const commentSection = {
+      comment: comment,
+      id: userid,
+      postID: viewBooks[viewBooks.length - 1].id,
+    };
+
+    console.log(commentSection);
+
+    axios
+      .post(`http://${window.location.hostname}:3001/comments`, commentSection)
+      .then((response) => {
+        if (response.data.comment) {
+          console.log(response.data.comment);
+
+          console.log(response.data.results);
+        }
+      })
+      .catch((e) => console.log(e));
+
+    // setComments([...comments, comment]);
+
     setComment('');
+
+    fetchComments();
   };
+
+  let postID = viewBooks[viewBooks.length - 1].id;
+
+  async function fetchComments() {
+    console.log(postID);
+    const res = await axios.get(
+      `http://${window.location.hostname}:3001/comments/${postID}`
+    );
+    console.log(res.data);
+    if (res.data) {
+      setComments(res.data);
+    }
+    // } else {
+    //   return;
+    // }
+  }
+
+  React.useEffect(() => {
+    fetchComments();
+  }, []);
+
+  React.useEffect(() => {
+    fetchComments();
+  }, [comment]);
 
   // Function to add books posts to cart
   const handleAddCart = () => {
@@ -69,7 +122,6 @@ const ViewBook = () => {
     );
   };
 
-
   // make first letter of username to uppercase
   const viewBooksUsername = viewBooks[viewBooks.length - 1].name;
   const sellerid = viewBooks[viewBooks.length - 1].sellerid;
@@ -80,14 +132,11 @@ const ViewBook = () => {
   dispatch(setSellerId(sellerid));
   dispatch(setSellerEmail(viewBooks[viewBooks.length - 1].sellerEmail));
 
-
-
   React.useEffect(() => {
     if (viewBooks[viewBooks.length - 1].type != 'tradebooks') {
-      setCommentDivStyle({display: 'none'});
+      setCommentDivStyle({ display: 'none' });
     }
   }, [viewBooks, 1]);
-    
 
   return (
     <div className="viewbook_container">
@@ -144,51 +193,62 @@ const ViewBook = () => {
               Add to cart
             </Button>
 
+            <div
+              key=""
+              className="comment-box"
+              id="comment_box"
+              style={commentDivStyle}
+            >
+              <h4 className="leave__comment">
+                {loggedIn ? 'Leave a comment' : 'Sign in to leave a comment'}
+              </h4>
+              {loggedIn && (
+                <div
+                  id="comments-box"
+                  className="each__comment__div"
+                  style={{ overflowY: 'scroll' }}
+                >
+                  {comments.map((message, i) => {
+                    return (
+                      <div className="comment__indiv" key={i}>
+                        <span className="comment__username">
+                          {message.name.charAt(0).toUpperCase() +
+                            message.name.slice(1)}
+                        </span>
 
-
-
-            <div key="" className="comment-box" id="comment_box" style={commentDivStyle}> 
-              <div
-                id="comments-box"
-                className="each__comment__div"
-                style={{ overflowY: 'scroll' }}>
-
-                <h4 className="leave__comment">{loggedIn ? "Leave a Comment" : "Comments"}</h4>
-                    
-                  {comments.map((message) => {
-                      return (
-                        <div>
-                          <span className="each__comment">{message}</span>
-                          <ScrollMessages messages={message} />
-                        </div>
-                      );
-                    })}
+                        <span className="each__comment">{message.comment}</span>
+                        <ScrollMessages messages={message.comment} />
+                      </div>
+                    );
+                  })}
                 </div>
+              )}
 
-                {loggedIn ?
-                  <div>
-                    <input
-                      id="comment"
-                      placeholder="Add a comment here"
-                      type="text"
-                      value={comment}
-                      name="comment"
-                      className="comment__box"
-                      onChange={(e) => setComment(e.target.value)}
-                    />
-                    <div classname="commentbox-buttons">
-                      <Button
-                        onClick={handleComment}
-                        id="addcomment"
-                        classname="comment-button"
-                      >
-                        {' '}
-                        send
-                      </Button>
-                    </div> 
-                  </div> 
-                : "" }
-         
+              {loggedIn ? (
+                <div className="comment__input">
+                  <input
+                    id="comment"
+                    placeholder="Add a comment here ...."
+                    type="text"
+                    value={comment}
+                    name="comment"
+                    className="comment__box"
+                    onChange={(e) => setComment(e.target.value)}
+                  />
+                  <div classname="commentbox-buttons">
+                    <Button
+                      onClick={handleComment}
+                      id="addcomment"
+                      classname="comment-button"
+                    >
+                      {' '}
+                      <SendIcon></SendIcon>
+                    </Button>
+                  </div>
+                </div>
+              ) : (
+                ''
+              )}
             </div>
           </div>
         </div>
